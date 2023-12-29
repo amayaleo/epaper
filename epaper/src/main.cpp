e0a52bbf2dbdc8122b1e4ded79845bbe1feacaff
bee <bee@macbook.local>
2023-12-29 19:16:52 -0300

    update 202312291916

--- epaper/src/main.cpp
+++ epaper/src/main.cpp
@@ -8,19 +8,19 @@
 
 
 int main(){
+std::unique_ptr<EPAPER::EPD_Driver>epaper {std::make_unique<EPAPER::EPD_Driver>((eScreen_EPD_213, EPAPER::boardRaspberryPi_EXT)};
+    //EPAPER::EPD_Driver epaper(eScreen_EPD_213, EPAPER::boardRaspberryPi_EXT);
+                // EPD_Driver epdtest(eScreen_EPD_271, boardRaspberryPiPico_RP2040_EXT3);
 
-    EPAPER::EPD_Driver epaper(eScreen_EPD_213, EPAPER::boardRaspberryPi_EXT);
-    // EPD_Driver epdtest(eScreen_EPD_271, boardRaspberryPiPico_RP2040_EXT3);
-
-    // Initialize CoG
-    epaper.COG_initial();
-    // Global Update Call
-    // void EPD_Driver::globalUpdate(const uint8_t * data1s, const uint8_t * data2s)
-    epaper.globalUpdate( BW_monoBuffer, BW_0x00Buffer);
-    TYME::delay(100);
-    epaper.globalUpdate(BWR_blackBuffer,BWR_redBuffer);
-    // Turn off CoG
-    epaper.COG_powerOff();
+            // Initialize CoG
+//epaper.COG_initial();
+            // Global Update Call
+            // void EPD_Driver::globalUpdate(const uint8_t * data1s, const uint8_t * data2s)
+//epaper.globalUpdate( BW_monoBuffer, BW_0x00Buffer);
+TYME::delay(100);
+//epaper.globalUpdate(BWR_blackBuffer,BWR_redBuffer);
+          // Turn off CoG
+//epaper.COG_powerOff();
 
     return 0;
 }
